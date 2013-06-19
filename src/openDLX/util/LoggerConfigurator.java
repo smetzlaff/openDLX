46023242b4cbdbaa88c9fe8250b4dde442dab991
Stefan Metzlaff <stefan.metzlaff@gmail.com>
2013-06-19 15:13:04 +0200

    Minor changes (doxygen version and imports)

--- src/openDLX/util/LoggerConfigurator.java
+++ src/openDLX/util/LoggerConfigurator.java
@@ -20,8 +20,6 @@
  ******************************************************************************/
 package openDLX.util;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.Properties;
